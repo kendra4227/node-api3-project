bd79cc52b7865433784fcd8bf9b278086f9ca6d8
Lakendria <kendra939@gmail.com>
2021-06-09 18:18:07 -0400

    MVP

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-const server = require('./api/server')
+const express = require("express");
+const server = express();
+const logger = require('./middleware/logger');
+const welcomeRouter = require('./routers/welcome');
+const userRouter = require("./users/userRouter");
+const postRouter = require('./posts/postRouter');
 
-server.listen(4000, ()=>{
-    console.log('\n* Server Running on http://localhost:4000 *\n')
-})
+server.use(logger());
+
+server.use(express.json());
+const host = process.env.HOST || "0.0.0.0";
+const port = process.env.PORT || 8000;
+
+server.use("/", welcomeRouter);
+server.use("/users", userRouter);
+server.use("/posts", postRouter);
+
+server.listen(port, () => {
+  console.log(`\n*** Server listening on ${host}:${port}`);
+}); 
+
